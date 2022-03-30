100fe2727dea2ee0748330c783d6774ec0131504
Sareh Almaaz <sa@voidmod.com>
2022-03-30 09:43:26 +0200

    remove env file

--- remix.init/index.js
+++ remix.init/index.js
@@ -3,7 +3,6 @@ const crypto = require("crypto");
 const fs = require("fs/promises");
 const path = require("path");
 const inquirer = require("inquirer");
-
 const sort = require("sort-package-json");
 
 function escapeRegExp(string) {
@@ -17,8 +16,6 @@ function getRandomString(length) {
 
 async function main({ rootDirectory }) {
   const README_PATH = path.join(rootDirectory, "README.md");
-  const EXAMPLE_ENV_PATH = path.join(rootDirectory, ".env.example");
-  const ENV_PATH = path.join(rootDirectory, ".env");
   const PACKAGE_JSON_PATH = path.join(rootDirectory, "package.json");
 
   const REPLACER = "the-beatles-stack-template";
@@ -30,16 +27,10 @@ async function main({ rootDirectory }) {
     // get rid of anything that's not allowed in an app name
     .replace(/[^a-zA-Z0-9-_]/g, "-");
 
-  const [prodContent, readme, env, packageJson] = await Promise.all([
+  const [readme, packageJson] = await Promise.all([
     fs.readFile(README_PATH, "utf-8"),
-    fs.readFile(EXAMPLE_ENV_PATH, "utf-8"),
     fs.readFile(PACKAGE_JSON_PATH, "utf-8"),
   ]);
-
-  const newEnv = env.replace(
-    /^SESSION_SECRET=.*$/m,
-    `SESSION_SECRET="${getRandomString(16)}"`
-  );
 
   const newReadme = readme.replace(
     new RegExp(escapeRegExp(REPLACER), "g"),
@@ -55,7 +46,6 @@ async function main({ rootDirectory }) {
 
   await Promise.all([
     fs.writeFile(README_PATH, newReadme),
-    fs.writeFile(ENV_PATH, newEnv),
     fs.writeFile(PACKAGE_JSON_PATH, newPackageJson),
   ]);
 
@@ -78,6 +68,25 @@ async function main({ rootDirectory }) {
 Start development with \`npm run dev\`
     `.trim()
   );
+}
+
+async function askSetupQuestions({ rootDirectory }) {
+  const answers = await inquirer.prompt([
+    {
+      name: "validate",
+      type: "confirm",
+      default: false,
+      message:
+        "Do you want to run the build/tests/etc to verify things are setup properly?",
+    },
+  ]);
+
+  if (answers.validate) {
+    console.log(
+      `Running the validate script to make sure everything was set up properly`
+    );
+    execSync(`npm run validate`, { stdio: "inherit", cwd: rootDirectory });
+  }
 }
 
 module.exports = main;
